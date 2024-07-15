fbdd91bf7671dcc890217cab923d25dce960ad2a
aniket701661 <aniketchandivkar11@gmail.com>
2024-07-16 02:10:42 +0530

    changed cv file

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 <motion.span initial={{opacity:0,scale:0.5}} animate={{opacity:1,scale:1}} transition={{duration:0.5}}>Aniket Dev</motion.span>
 <div className='social'>
   <a href="https://www.linkedin.com/in/aniket-chandivkar-7611992b4/" target="blank">
-  <a className="cv" target="blank" href="https://drive.google.com/file/d/1o_5So8yB-hF_z9kxGxB3qoEXV-cLsom5/view?usp=sharing" download="Your_Name_CV.pdf">Download CV</a> <img src="/linkedin.png" alt="" /></a>
+  <a className="cv" target="blank" href="https://drive.google.com/file/d/1XiCSZ5cVXpVbJ0jQBzJDCdUbzIlfS-T9/view?usp=sharing" download="Your_Name_CV.pdf">Download CV</a> <img src="/linkedin.png" alt="" /></a>
     <a href="https://www.instagram.com/_aniket10__/" target="blank"> <img src="/instagram.png" alt="" /></a>
     <a href="https://github.com/AniketCode10" target="blank"> <img src="/social.png" alt="" /></a>
    </div>
